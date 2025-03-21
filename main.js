c6dcd4a7fed88a6d5e0c7edb25a12e67ad51c203
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-03-21 12:17:19 -0500

    feat: :sparkles: Basic logic done

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("ok")
+
+var character = prompt("Hello, please enter your promp: ")
+
+function limitbrackets(character){
+
+
+    character = character.toString()
+
+    let end = (character.length-1)
+
+    console.log(character[0] + character[end])
+    
+
+   if (character[0] == "{" &&  character[end] ==  "}" ) {
+    console.log("{ }" + "esta ordenado")
+        
+   } else{
+    console.log("no esta ordenado")
+   }
+
+    console.log(character)
+
+}
+
+
+console.log(limitbrackets(character))
